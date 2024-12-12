05c45bb731dfafd72887566a54df292630d5914c
santhu-tech <psanthiyamahe@gmail.com>
2024-12-12 15:19:19 +0530

    Day-4-Event Mgt & Multithreading

---
+++ EventMgt/Day4/EventMgt.java
@@ -0,0 +1,40 @@
+package com.EventMgt.Day4;
+
+public class EventMgt {
+    //SOLID PRINICIPLE ,Design patterns,Exception handling ,Collection framework,Functional Programming
+    // Stream API
+
+    //Todo-1:
+    //Event Management System
+    //Core Modules :
+    //Event Scheduling Module
+    //Attendee MAgt Module
+    //Budget Tracking Module
+
+    //Todo-2
+    //Event schduling module -
+    //Create Event Class --Attributes:Name,Date,Venue
+    //Create the sub classes like corporate event,conference event ,famil event
+    //Create EventService Class -->Create method to schedule the event -->scheduleEVent
+    //Create the Main class -->
+
+
+
+    //sample output:
+    //Event scheduled successfully
+    //name :Corporate Event
+    //Date:12.12.2024
+    //Venue:Grand Hall
+    //Error msg:Venue already booked on those dates
+    //All event scheduled for the particular date
+    //name :Corporate Event
+    //Date:12.12.2024
+    //Venue:Grand Hall
+
+    //Name :Conference event
+    //Date :12.12.2024
+    //Venue:"Royal palace"
+
+
+}
+
